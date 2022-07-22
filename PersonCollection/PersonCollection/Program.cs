e2b01d5ff2ba93198855113355e9945fb516ec13
Ihor Trubachov <2109qwaszx.igor.2109qwaszx@gmail.com>
2022-07-22 14:04:06 +0200

    Update Program.cs

--- PersonCollection/PersonCollection/Program.cs
+++ PersonCollection/PersonCollection/Program.cs
@@ -7,28 +7,7 @@ namespace PersonCollection
     {
         static void Main(string[] args)
         {
-            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-            MyStack<int> vs = new MyStack<int>();
-
-
-            for (int i = 0; i < 11; i++)
-            {
-                vs.Push(i + 1);
-            }
-
-
-
-            foreach (var item in vs)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine();
-
-            //foreach (var item in vs)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            
         }
     }
 }
